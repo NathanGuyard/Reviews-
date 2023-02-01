f93ed0613c7fc8101113a4e2fd2e505ada2bf17c
Nathanguyard <nathan.arkanoxd12@gmail.com>
2023-02-01 18:14:37 +0100

    Project

---
+++ src/components/App/index.js
@@ -0,0 +1,19 @@
+import './style.css';
+import React from 'react';
+import Review from '../Reviews/';
+
+
+function App() {
+  return <main>
+    <section className='container'>
+      <div className='title'>
+        <h2>Ours Reviews</h2>
+        <div className='underline'></div>
+      </div>
+      <Review />
+    </section>
+  </main>;
+
+}
+
+export default App;
